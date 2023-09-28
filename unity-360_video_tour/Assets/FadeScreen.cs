89f55c0e41dbbbdb69a03c9df52414f136a585f1
mwblevins <79066736+mwblevins@users.noreply.github.com>
2023-09-28 11:52:13 -0500

    completed fader

--- unity-360_video_tour/Assets/FadeScreen.cs
+++ unity-360_video_tour/Assets/FadeScreen.cs
@@ -12,24 +12,20 @@ public class FadeScreen : MonoBehaviour
         
     }
 
-    public IEnumerator FadeBlackOutSquare(bool fadeToBlack = true, int fadeSpeed = 2)
+    public IEnumerator FadeBlackOutSquare(int fadeSpeed = 1)
     {
         Color objectColor = blackOutSquare.GetComponent<RawImage>().color;
         float fadeAmount;
-
-        if (fadeToBlack)
-        {
             while (blackOutSquare.GetComponent<RawImage>().color.a < 1)
             {
                 fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
 
-                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                objectColor.a += fadeSpeed * Time.deltaTime;
+                Debug.Log(objectColor);
                 blackOutSquare.GetComponent<RawImage>().color = objectColor;
                 yield return null;
             }
-        }
-        else
-        {
+            yield return new WaitForSeconds(2.0f);
             while (blackOutSquare.GetComponent<RawImage>().color.a > 0)
             {
                 fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
@@ -37,7 +33,6 @@ public class FadeScreen : MonoBehaviour
                 objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
                 blackOutSquare.GetComponent<RawImage>().color = objectColor;
                 yield return null;
-            } 
-        }
+            }
     }
 }
